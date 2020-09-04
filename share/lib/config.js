cdde324275c46d6c4b8952bb424e1fbe31114e7e
aMehGit <70306382+aMehGit@users.noreply.github.com>
2020-09-04 23:45:36 +0100

    Update config.js

--- share/lib/config.js
+++ share/lib/config.js
@@ -401,7 +401,7 @@ const COLUMN_INDEX = {
                       (conf.format.use_tailing_pct? '<small>%</small>' : '')
     },
     last20crit: {
-      v: _ => index30 + 1,
+      v: _ => last30Crit,
       f: (_, conf) => _.toFixed(conf.format.significant_digit.critical) +
                       (conf.format.use_tailing_pct? '<small>%</small>' : '')
     },
