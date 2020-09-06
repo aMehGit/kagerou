1401cb790b3b1db8ac26cdce13622cfb53a9f7c2
aMehGit <70306382+aMehGit@users.noreply.github.com>
2020-09-06 18:34:26 +0100

    Update config.js

--- share/lib/config.js
+++ share/lib/config.js
@@ -8,8 +8,8 @@ const CONFIG_DEFAULT = {
   lang: 'en',
   stats: {
     crit_chance: 0.05,
-    dh_chance: 0.0,
-    crit_dh_chance: 0.0
+    dh_chance: 0.00,
+    crit_dh_chance: 0.00
   },
   style: {
     // body
